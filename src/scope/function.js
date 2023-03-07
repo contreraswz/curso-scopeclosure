bfae0c4ce483af160bd8fd57650c9826d497380a
Contreraswz <contreraswz@gmail.com>
2023-03-07 06:30:05 -0700

    agergando function scope

---
+++ src/scope/function.js
@@ -0,0 +1,10 @@
+function saludos(){
+    let userName='Edwin';
+    console.log(userName);
+    if(userName==='Edwin'){
+        console.log(`Hola ${userName}`);
+    }
+}
+saludos();
+
+console.log(userName);
